2feb335164c3280d033d70bb23ccda81aa7fdc57
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2020-07-02 12:31:36 -0300

    Implementado insert()

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,6 +26,17 @@ public class Program {
 		System.out.println("\n===== TEST 3 - Seller findAll() =====");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n===== TEST 4 - Seller insert() =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new ID: "+newSeller.getId());
+		
 	}
 
 }
+
+
+
+
+
