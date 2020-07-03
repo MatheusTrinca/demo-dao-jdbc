bdb9efe065130d627aaa79fb52bbee416aee6c05
Matheus Felipe Trinca <matheusfelipetrinca@gmail.com>
2020-07-03 13:50:50 -0300

    Implementacoes da classe DepartmentDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		/*
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===== TEST 1 - Seller findById() =====");
@@ -41,7 +43,43 @@ public class Program {
 		System.out.println("\n===== TEST 6 - Seller delete() =====");
 		sellerDao.deleteById(8);
 		System.out.println("Deleted OK");
+		*/
 		
+		
+		/*
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("===== TEST 1 - Seller findById() =====");
+		Department dep = departmentDao.findById(2);
+		System.out.println(dep);
+		
+		System.out.println("\n===== TEST 2 - Department findAll() =====");
+		List<Department> deps = departmentDao.findAll();
+		deps.forEach(System.out::println);
+				
+		System.out.println("\n===== TEST 3 - Department insert() =====");
+		Department dep1 = new Department(null, "Comidas");
+		departmentDao.insert(dep1);
+		System.out.println("Department Id: "+dep1.getId());
+		System.out.println(departmentDao.findById(dep1.getId()));
+
+		
+		System.out.println("\n===== TEST 4 - Department update() =====");
+		Department dep2 = departmentDao.findById(8);
+		dep2.setName("Instrumentos");
+		departmentDao.update(dep2);
+		System.out.println("Update OK");
+		
+		System.out.println("\n===== TEST 5 - Department deleteById() =====");
+		departmentDao.deleteById(8);
+		System.out.println("Deleted OK");
+		
+		
+
+		
+
+		
+		 */
 	}
 
 }
